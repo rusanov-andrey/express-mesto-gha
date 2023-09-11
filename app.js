47853d742bf9436f63cb2164cf5413ce768267dd
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-09-12 00:13:53 +0300

    Доработка API

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.post('/signin', login);
 app.post('/signup', celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
